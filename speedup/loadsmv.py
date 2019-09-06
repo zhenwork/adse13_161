14814feb68da0408841350dd5b92a9bb72c148e2
zhenwork <36317218+zhenwork@users.noreply.github.com>
2019-09-06 00:52:28 -0700

    Update loadsmv.py

--- speedup/loadsmv.py
+++ speedup/loadsmv.py
@@ -1,7 +1,20 @@
 import re
+import gzip 
 
 def loadsmv(fname):
     ## \nSIZE1=1739;\nSIZE2=1748
+    if fname.endswith(".gz"):
+        with gzip.open(fname, "rb") as f:
+            raw = f.read()
+        h = raw[0:1024]
+        d = raw[1024:]
+        f.close()
+        sx = int(re.search("SIZE2=\d+",h).group().split("=")[1])
+        sy = int(re.search("SIZE1=\d+",h).group().split("=")[1])
+        flat_image = np.frombuffer(d, dtype=np.uint16)
+        image = np.reshape(flat_image, ((sx,sy)) ).astype(float)[::-1].T
+        return image
+
     f = open(fname,"rb")
     raw = f.read()
     h = raw[0:1024]
